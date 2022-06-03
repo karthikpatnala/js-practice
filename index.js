0c3567a214ed4ff6a572db1918080eaab1e01412
karthik patnala <karthik.194625@gmail.com>
2022-06-03 17:02:43 +0530

    newjs

---
+++ index.js
@@ -0,0 +1,22 @@
+const points = [40, 100, 1, 5, 25, 10];
+/*function myarraymin(points){
+
+    return Math.max.apply(null,points);
+}
+console.log(myarraymin(points)*/
+//toString(points.forEach(item=>console.log((item))));//
+/*const pt=points.map(myfunc);
+function myfunc(value){
+ return value*2
+}
+console.log(pt)*/
+/*let x= points.reduce(myfunc);
+function myfunc(total,value){
+    return total+value;
+}*/
+/*const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const f = fruits.entries();
+console.log(f)*/
+var age = 20;
+var beverage = (age >= 21) ? "Beer" : "Juice";
+console.log(beverage); // "Beer"
